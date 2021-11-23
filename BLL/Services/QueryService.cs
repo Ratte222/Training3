5cbb728b66d7326d1d8c29f63f80df9cf751e219
Ratte222 <Artur21111999.o@gmail.com>
2021-11-23 15:43:43 +0300

    work version

--- BLL/Services/QueryService.cs
+++ BLL/Services/QueryService.cs
@@ -22,41 +22,20 @@ namespace BLL.Services
         //https://docs.microsoft.com/en-us/ef/core/querying/raw-sql
         public void SQLQuery()
         {
-            #region EF Core 5+
-            //var pupilIds = _appDBContext.Pupils.AsNoTracking()
-            //    .Include(i => i.PupilAcademicSubjects.Where(j => j.AcademicSubject.Title.ToLower() == "history"))
-            //    .ToArray().Where(n => n.PupilAcademicSubjects.Count > 0).Select(j=>j.Id).ToArray();
-            //var temp_ = _appDBContext.SchoolClasses.Include(i => i.Pupils.Where(j => pupilIds.Any(n => n == j.Id)))
-            //    .ToArray();
-
-            //string query = @"SELECT sc.Id, sc.Title, p1.pId, p1.FirstName, p1.LastName, p1.SchoolClassId, " +
-            //    @"p1.PupilId, p1.AcademicSubjectId, p1.aId, p1.aTitle, p1.Description " +
-            //    @"FROM schoolclasses AS `sc` " +
-            //    @"INNER JOIN ( " +
-            //    @"SELECT p.Id AS pId, p.FirstName, p.LastName, p.SchoolClassId, pas0.PupilId, pas0.AcademicSubjectId, " +
-            //        @"pas0.aId, pas0.Title AS aTitle, pas0.Description FROM pupils AS `p` " +
-            //        @"INNER JOIN ( " +
-            //        @"SELECT pas.PupilId, pas.AcademicSubjectId, a1.aId, a1.Title, a1.Description FROM pupilacademicsubjects AS `pas` " +
-            //            @"INNER JOIN ( " +
-            //            @"SELECT a0.Id AS aId, a0.Title, a0.Description FROM academicsubjects AS `a0` " +
-            //                @"WHERE a0.Title = 'Chemistry' " +
-            //            @") AS `a1` ON a1.aId = pas.AcademicSubjectId " +
-            //        @") AS `pas0` ON p.Id = pas0.PupilId " +
-            //    @") AS `p1` ON sc.Id = p1.SchoolClassId";
-            ////var temp = _appDBContext.SchoolClasses.FromSqlInterpolated($"SELECT `sc`.`Id`, `sc`.`Title`, `s`.`Id0`, `s`.`FirstName`, `s`.`LastName`, `s`.`SchoolClassId` FROM ( SELECT `p`.`Id` AS `Id0`, `p`.`FirstName`, `p`.`LastName`, `p`.`SchoolClassId` FROM pupils AS `p` WHERE `p`.`FirstName` = 'Taya' ) AS `s` LEFT JOIN schoolclasses AS `sc` ON `sc`.`Id` = `s`.`SchoolClassId`"
-            ////              ).ToArray();
-            //var schoolClass = _appDBContext.SchoolClasses.FromSqlRaw(query)
-            //    .AsNoTracking().AsEnumerable()
-            //    .GroupBy(i => i.Id
-            //    //j=>j, (key, entity)=>
-            //    //new {
-            //    //    SchoolClass = entity
-            //    //}
-            //    );
-            #endregion
-
-            
-            
+            string studySubject = "Chemistry";
+            var pupilsIds = _appDBContext.AcademicSubjects
+                .Where(i => i.Title.ToLower() == studySubject.ToLower())
+                .Include(j => j.PupilAcademicSubjects)
+                .Select(i=>i.PupilAcademicSubjects.Select(n=>n.PupilId)).ToArray();
+            //bool temp = pupilsIds[0].Any(i => i == 5);
+            if(pupilsIds.Length > 0)
+            {
+                var schoolClasses = (from sc in _appDBContext.SchoolClasses
+                                     orderby sc.Id
+                                     join p in _appDBContext.Pupils on sc.Id equals p.SchoolClassId
+                                     where sc.Id == p.SchoolClassId && pupilsIds[0].Any(i => i == p.Id)
+                                     select sc).Distinct().ToArray();
+            }            
         }
 
 
