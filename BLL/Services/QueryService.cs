4409477fd1415851b3a62cccead8a66a0cc913c0
Ratte222 <Artur21111999.o@gmail.com>
2021-11-23 13:34:38 +0300

    downgraded the libraries version to 2.1

--- BLL/Services/QueryService.cs
+++ BLL/Services/QueryService.cs
@@ -28,22 +28,10 @@ namespace BLL.Services
                 .ToArray().Where(n => n.PupilAcademicSubjects.Count > 0).Select(j=>j.Id).ToArray();
             var temp_ = _appDBContext.SchoolClasses.Include(i => i.Pupils.Where(j => pupilIds.Any(n => n == j.Id)))
                 .ToArray();
-            #endregion
-            // Work in Heidi 
-            //SELECT x0.Id, x0.Title, y0.Id, y0.FirstName, y0.LastName, y0.SchoolClassId, y1.PupilId, y1.AcademicSubjectId, z0.Id, z0.Title, z0.Description
-            //FROM schoolclasses AS x0 LEFT JOIN pupils AS y0 ON x0.Id = y0.SchoolClassId
-            //LEFT JOIN pupilacademicsubjects AS y1 ON y1.PupilId = y0.Id
-            //LEFT JOIN academicsubjects AS z0 ON z0.Id = y1.AcademicSubjectId
-            //WHERE z0.Title = 'Chemistry'
+
             
-            string query = @"SELECT `sc`.`Id`, `sc`.`Title`, `s`.`Id0`, `s`.`FirstName`, `s`.`LastName`, `s`.`SchoolClassId` FROM ( "
-                        + @"SELECT `p`.`Id` AS `Id0`, `p`.`FirstName`, `p`.`LastName`, `p`.`SchoolClassId` FROM pupils AS `p` "
-                        + @"WHERE `p`.`FirstName` = 'Taya'"
-                        + @") AS `s`"
-                        + @"LEFT JOIN schoolclasses AS `sc` ON `sc`.`Id` = `s`.`SchoolClassId` ";
-
-
-            query = @"SELECT sc.Id, sc.Title, p1.pId, p1.FirstName, p1.LastName, p1.SchoolClassId, " +
+            #endregion
+            string query = @"SELECT sc.Id, sc.Title, p1.pId, p1.FirstName, p1.LastName, p1.SchoolClassId, " +
                 @"p1.PupilId, p1.AcademicSubjectId, p1.aId, p1.aTitle, p1.Description " +
                 @"FROM schoolclasses AS `sc` " +
                 @"INNER JOIN ( " +
