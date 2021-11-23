9f81f27f2c8a26095c0543ef14248f6e2ba67c85
Ratte222 <Artur21111999.o@gmail.com>
2021-11-23 15:12:43 +0300

    update sql query

--- BLL/Services/QueryService.cs
+++ BLL/Services/QueryService.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using Microsoft.Data.SqlClient;
 using System.Text;
 using DAL.Entity;
+using MySqlConnector;
 
 namespace BLL.Services
 {
@@ -23,11 +24,15 @@ namespace BLL.Services
         public void SQLQuery()
         {
             #region EF Core 5+
+            string studySubject_ = "Chemistry";
             var pupilIds = _appDBContext.Pupils.AsNoTracking()
-                .Include(i => i.PupilAcademicSubjects.Where(j => j.AcademicSubject.Title.ToLower() == "history"))
-                .ToArray().Where(n => n.PupilAcademicSubjects.Count > 0).Select(j=>j.Id).ToArray();
+                .Include(i => i.PupilAcademicSubjects.Where(j => j.AcademicSubject.Title.ToLower() == studySubject_.ToLower()))
+                .ToArray().Where(n => n.PupilAcademicSubjects.Count > 0).Select(j => j.Id).ToArray();
             var temp_ = _appDBContext.SchoolClasses.Include(i => i.Pupils.Where(j => pupilIds.Any(n => n == j.Id)))
                 .ToArray();
+
+            
+            MySqlParameter studySubjectParameter = new MySqlParameter("@studySubject", studySubject_);
 
             string query = @"SELECT sc.Id, sc.Title, p1.pId, p1.FirstName, p1.LastName, p1.SchoolClassId, " +
                 @"p1.PupilId, p1.AcademicSubjectId, p1.aId, p1.aTitle, p1.Description " +
@@ -39,18 +44,13 @@ namespace BLL.Services
                     @"SELECT pas.PupilId, pas.AcademicSubjectId, a1.aId, a1.Title, a1.Description FROM pupilacademicsubjects AS `pas` " +
                         @"INNER JOIN ( " +
                         @"SELECT a0.Id AS aId, a0.Title, a0.Description FROM academicsubjects AS `a0` " +
-                            @"WHERE a0.Title = 'Chemistry' " +
+                            @"WHERE a0.Title = @studySubject " +
+                            //@"WHERE a0.Title = 'Chemistry' " +
                         @") AS `a1` ON a1.aId = pas.AcademicSubjectId " +
                     @") AS `pas0` ON p.Id = pas0.PupilId " +
                 @") AS `p1` ON sc.Id = p1.SchoolClassId";
-            var schoolClass = _appDBContext.SchoolClasses.FromSqlRaw(query)
-                .AsNoTracking().AsEnumerable()
-                .GroupBy(i => i.Id
-                //j=>j, (key, entity)=>
-                //new {
-                //    SchoolClass = entity
-                //}
-                );
+            var schoolClass = _appDBContext.SchoolClasses.FromSqlRaw(query, studySubjectParameter)
+                .Distinct().AsEnumerable();                
             #endregion
 
 
